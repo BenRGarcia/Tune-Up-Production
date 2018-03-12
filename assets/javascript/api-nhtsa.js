a918c2fc32600abad262c500f1162d3d2c8bcbbd
Ben Garcia <ben@seebenprogram.com>
2018-03-12 04:08:49 -0400

    Add console log for debugging

--- assets/javascript/api-nhtsa.js
+++ assets/javascript/api-nhtsa.js
@@ -42,6 +42,7 @@ const vehicleApi = {
     return this._modelObjArray;
   },
   searchModel(year, make) {
+    console.log("Searching for models...");
     // Ignore 'falsy' 'make' arg passed
     if (make) {
       // Convert year argument to string (in case passed as type number)
