b7275cdb78d809c3e628766e5076266f6c72453f
Ben Garcia <ben@seebenprogram.com>
2018-03-12 06:41:40 -0400

    Revert redirect link

--- assets/javascript/firebase-auth.js
+++ assets/javascript/firebase-auth.js
@@ -5,7 +5,7 @@
 
 // FirebaseUI config.
 var uiConfig = {
-  signInSuccessUrl: 'localhost:8888/virtual-garage.html',
+  signInSuccessUrl: 'https://benrgarcia.github.io/Tune-Up-Production/virtual-garage.html',
   signInOptions: [
     firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
