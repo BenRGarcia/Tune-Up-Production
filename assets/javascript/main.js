320f089ee60b35ee45b4e555c4231276255b9900
Ben Garcia <ben@seebenprogram.com>
2018-03-12 18:24:40 -0400

    Re-enable not signed in redirect

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -23,7 +23,7 @@ function initializeGarage() {
       });
     } else {
       // User not signed in, redirect to home page
-      // window.location.replace("https://benrgarcia.github.io/Tune-Up-Production/")
+      window.location.replace("https://benrgarcia.github.io/Tune-Up-Production/")
     }
   });
 }
